293ec2471d074ccc7f04e167d0d889a2d12d24c4
Jérôme <jerome@2le.net>
2018-10-11 11:09:39 +0200

    Execute only active trigger

--- src/Service/TriggerExecutor.php
+++ src/Service/TriggerExecutor.php
@@ -1,25 +1,28 @@
 <?php
-
 namespace Lle\BpmBundle\Service;
-
 
 use Doctrine\ORM\EntityManagerInterface;
 use Lle\BpmBundle\Entity\Trigger;
 use Lle\BpmBundle\Service\Tag\ActionChain;
 use Lle\BpmBundle\Service\Tag\TriggerChain;
 
-class TriggerExecutor {
-
+class TriggerExecutor
+{
 
     private $triggerChain;
+
     private $actionChain;
+
     private $triggerRepository;
-     /**
-     * @var EntityManagerInterface 
+
+    /**
+     *
+     * @var EntityManagerInterface
      */
     private $em;
 
-    public function __construct(TriggerChain $triggerChain, ActionChain $actionChain, EntityManagerInterface $em ){
+    public function __construct(TriggerChain $triggerChain, ActionChain $actionChain, EntityManagerInterface $em)
+    {
         $this->triggerRepository = $em->getRepository(Trigger::class);
         $this->triggerChain = $triggerChain;
         $this->actionChain = $actionChain;
@@ -28,16 +31,23 @@ class TriggerExecutor {
 
     public function executeAll()
     {
-        foreach($this->triggerRepository->findAll() as $trigger) {
+        foreach ($this->triggerRepository->findBy([
+            'active' => true
+        ]) as $trigger) {
             $this->executeTriggerOnRepo($trigger);
         }
     }
+
     public function executeTriggerOnRepo($trigger)
     {
+        if (!$trigger->getActive()) {
+            return;
+        }
         $i = 0;
-        foreach($this->em->getRepository($trigger->getEntityClass())->findByEtat($trigger->getFrom()) as $object) {
+        foreach ($this->em->getRepository($trigger->getEntityClass())
+            ->findByEtat($trigger->getFrom()) as $object) {
             $this->executeTrigger($object, $trigger);
-            if ($i++ % 50 == 0) {
+            if ($i ++ % 50 == 0) {
                 $this->em->flush();
             }
         }
@@ -46,28 +56,36 @@ class TriggerExecutor {
 
     public function execute($object)
     {
-        foreach($this->triggerRepository->findAll() as $trigger){
-            $this->executeTrigger($object, $trigger);        
+        foreach ($this->triggerRepository->findBy([
+            'active' => true
+        ]) as $trigger) {
+            $this->executeTrigger($object, $trigger);
         }
         $this->em->flush();
     }
-    
+
     public function executeOne($object, $triggerCode)
     {
-        $trigger = $this->triggerRepository->findOneByCode($triggerCode);
-        if ($this->executeTrigger($object, $trigger)) {        
+        $trigger = $this->triggerRepository->findOneBy([
+            'code' => $triggerCode,
+            'active' => true
+        ]);
+        if ($this->executeTrigger($object, $trigger)) {
             $this->em->flush();
             return true;
         }
         return false;
-    }    
-    
+    }
+
     public function executeTrigger($object, $trigger)
     {
+        if (!$trigger->getActive()) {
+            return;
+        }
         $typeTrigger = $this->triggerChain->getTrigger($trigger->getClassName());
         $typeTrigger->setParameters($trigger->getParameters() ?? []);
-        if ( $object->getEtat()==$trigger->getFrom() && $typeTrigger->shouldExecute($object)){
-            foreach($trigger->getActions() as $action){
+        if ($object->getEtat() == $trigger->getFrom() && $typeTrigger->shouldExecute($object)) {
+            foreach ($trigger->getActions() as $action) {
                 if ($action->getActive()) {
                     /* @var \Lle\BpmBundle\Entity\Action $action */
                     $typeAction = $this->actionChain->getAction($action->getClassName());
@@ -78,7 +96,7 @@ class TriggerExecutor {
             $object->setEtat($trigger->getTo());
             $object->setEtatUpdatedAt(new \Datetime());
             $this->em->persist($object);
-            return true;    
+            return true;
         }
         return false;
     }
